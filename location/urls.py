10c3fcecc9654003ebe8d9d64541634638946c6a
David Tjokroaminoto <dynamic.dante@gmail.com>
2015-03-20 00:21:08 +0800

    added location

---
+++ location/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, url
+from rest_framework.urlpatterns import format_suffix_patterns
+from location import views
+
+urlpatterns = patterns('',
+    url(r'^address/$', views.AddressList.as_view()),
+    url(r'^address/([0-9]+)/$', views.AddressDetail.as_view()),
+)
+
+urlpatterns = format_suffix_patterns(urlpatterns)
