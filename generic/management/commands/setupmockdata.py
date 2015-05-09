e5cb7e8dcaf1b271b4d65b921b507c2b77dd5ec6
David Tjokroaminoto <dynamic.dante@gmail.com>
2015-05-09 12:40:40 +0800

    added setup mock data script

---
+++ generic/management/commands/setupmockdata.py
@@ -0,0 +1,54 @@
+from django.core.management.base import BaseCommand, CommandError
+from rest_framework.authtoken.models import Token
+from generic.models import Category
+from member.models import Member, MemberCategory
+from location.models import Address, Area
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        member = Member.objects.create_superuser(
+            username='root',
+            password='root',
+            email='root@beer.com',
+            )
+
+        bidderone = Member.objects.create_superuser(
+            username='bidderone',
+            password='bidderone',
+            email='bidder@one.com',
+            )
+
+        biddertwo = Member.objects.create_superuser(
+            username='biddertwo',
+            password='biddertwo',
+            email='bidder@two.com',
+            )
+
+        Token.objects.create(user=bidderone)
+        Token.objects.create(user=biddertwo)
+        token = Token.objects.create(user=member,key='fa97ba8257bd480850c9f72fba6609e28acd619c')
+
+        area = Area.objects.create(
+            area_name='Bukit Batok'
+            )
+
+        address = Address.objects.create(
+            address_name='The Elitist',
+            address_line_1='25 Bukit Batok Crescent',
+            address_line_2='#09-12',
+            postal_code='658101',
+            owner=member,
+            area =area
+            )
+
+        cat_1 = Category.objects.create(title='Robbing')
+        cat_2 = Category.objects.create(title='Fishing')
+
+
+        MemberCategory.objects.create(member=member,category=cat_1)
+        MemberCategory.objects.create(member=member,category=cat_2)
+
+        MemberCategory.objects.create(member=bidderone,category=cat_1)
+        MemberCategory.objects.create(member=bidderone,category=cat_2)
+        MemberCategory.objects.create(member=biddertwo,category=cat_1)
+
